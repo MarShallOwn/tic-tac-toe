6c64f3af7d44cf403aab8636f76aa4f24a2e0870
MarShallOwn <53130198+MarShallOwn@users.noreply.github.com>
2020-05-01 15:09:42 +0200

    Updates

---
+++ precache-manifest.36ee82f9c25171be64bba437f7123429.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f50c6b0a6c8c38956329774e8c68bf9",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "cab3595f4a79bdb95e38",
+    "url": "/tic-tac-toe/static/css/main.91bf1d5e.chunk.css"
+  },
+  {
+    "revision": "491b8730582f492f9f27",
+    "url": "/tic-tac-toe/static/js/2.8bf7acdd.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/tic-tac-toe/static/js/2.8bf7acdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cab3595f4a79bdb95e38",
+    "url": "/tic-tac-toe/static/js/main.103d2809.chunk.js"
+  },
+  {
+    "revision": "6d0071f12963e7d48c55",
+    "url": "/tic-tac-toe/static/js/runtime-main.f092a1ad.js"
+  }
+]);
